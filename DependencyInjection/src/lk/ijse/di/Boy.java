d136be334787d670de057450c41a68da309e9ea7
sanuv9683 <sanuv9683@gmail.com>
2023-01-02 16:15:23 +0530

    Content Updated

--- DependencyInjection/src/lk/ijse/di/Boy.java
+++ DependencyInjection/src/lk/ijse/di/Boy.java
@@ -18,10 +18,16 @@ public class Boy {
 
     GoodGirl g;
 
-    public Boy(Girl g){//Constructor Injection
-        //we can use this method if we have the satisfied dependency Object while creating the Boy Object
-        this.g=g;
+//    public Boy(Girl g){//Constructor Injection
+//        //we can use this method if we have the satisfied dependency Object while creating the Boy Object
+//        this.g=g;
+//    }
+
+    public void setInjection(Girl girl){// setter method injection
+       //if we set a dependency using a setter method that is known as Setter Method injection
+        g=girl;
     }
+
     public void cattingWithGirl(){
         //Loos Coupling Applied
         g.chatting();
