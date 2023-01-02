ee24f9657a2eac9a63788f0b43b085d65905dc4e
sanuv9683 <sanuv9683@gmail.com>
2023-01-02 15:51:45 +0530

    Content Updated

--- DependencyInjection/src/lk/ijse/di/Boy.java
+++ DependencyInjection/src/lk/ijse/di/Boy.java
@@ -3,9 +3,14 @@ package lk.ijse.di;
 public class Boy {
 
     public void cattingWithGirl(){
+        //Loos Coupling Applied
         GoodGirl g= new Girl();
         g.chatting();
     }
+
+
+
+
 
     public static void main(String[] args) {
         Boy boy = new Boy();
