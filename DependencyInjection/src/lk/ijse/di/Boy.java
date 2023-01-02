143012f68007922c9c8765db6812944ef7b2476a
sanuv9683 <sanuv9683@gmail.com>
2023-01-02 16:07:03 +0530

    Content Updated with property injection and constructor injection

--- DependencyInjection/src/lk/ijse/di/Boy.java
+++ DependencyInjection/src/lk/ijse/di/Boy.java
@@ -1,10 +1,29 @@
 package lk.ijse.di;
 
 public class Boy {
+    //What is DI.?
+    // A mechanism of applying dependencies in a meaningful way
 
+
+    //DI Methods
+    //==============
+    //Property Injection
+    //Constructor Injection
+    //Setter method Injection
+    //Interface Through Injection
+
+
+    //we can use this if we know the dependency before we create the Boy Object
+    // GoodGirl g= new Girl();//DI property injection method
+
+    GoodGirl g;
+
+    public Boy(Girl g){//Constructor Injection
+        //we can use this method if we have the satisfied dependency Object while creating the Boy Object
+        this.g=g;
+    }
     public void cattingWithGirl(){
         //Loos Coupling Applied
-        GoodGirl g= new Girl();
         g.chatting();
     }
 
